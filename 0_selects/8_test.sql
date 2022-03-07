b5a5cefa72cbbbbb041ea24f4ac96e80bcb2cc84
princerequino <requino.prince@gmail.com>
2022-03-07 16:46:24 -0500

    add 3_queries

--- 0_selects/8_test.sql
+++ 0_selects/8_test.sql
@@ -12,3 +12,10 @@
 -- JOIN cohorts ON cohort_id = cohorts.id
 -- WHERE cohorts.start_date != students.start_date
 -- ORDER BY cohort_start_date;
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL 
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;
