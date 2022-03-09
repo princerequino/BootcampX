cf3e88300439ba27eb80f883f488f1bfe1a8995b
princerequino <requino.prince@gmail.com>
2022-03-09 13:43:33 -0500

    add SQL Inject security

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,18 @@ const pool = new Pool({
   port: 5432
 });
 
+const cohortName = process.argv[2] || 5;
+const values = [`%${cohortName}%`]
+
 pool.query(`
 SELECT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers JOIN assistance_requests
 ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
-ORDER BY teachers.name;`)
+ORDER BY teachers.name;`, values)
 .then (res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
